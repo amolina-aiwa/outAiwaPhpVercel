38e8cad4b93f30835bf447e7f6f27c11ccc57efc
Leandro Calvet <leandro@total.local>
2024-07-25 15:32:16 -0300

    ad

--- database/app.js
+++ database/app.js
@@ -1,25 +1,22 @@
 import express from 'express';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import db from './db.js';
 import audioRoutes from './routes/audioRoutes.js';
 import includeRoutes from './routes/includeRoutes.js';
 import tabletsRoutes from './routes/tabletsRoutes.js';
 import featuresRoutes from './routes/featuresRoutes.js';
-import featuredFeatures from './routes/featuredFeaturesRoutes.js';
+import featuredFeaturesRoutes from './routes/featuredFeaturesRoutes.js';
 import cooksRoutes from './routes/cooksRoutes.js';
 import headsetsRoutes from './routes/headsetsRoutes.js';
 import notebooksRoutes from './routes/notebooksRoutes.js';
 import discontinuosRoutes from './routes/discontinuosRoutes.js';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
-// Cargar variables de entorno
 dotenv.config();
 
 const app = express();
 const PORT = process.env.API_URL || 8000;
-
-console.log(PORT)
 
 app.use(cors());
 app.use(express.json());
@@ -27,18 +24,19 @@ app.use('/audio', audioRoutes);
 app.use('/includes', includeRoutes);
 app.use('/tablets', tabletsRoutes);
 app.use('/features', featuresRoutes);
-app.use('/featuredFeatures', featuredFeatures);
+app.use('/featuredFeatures', featuredFeaturesRoutes);
 app.use('/cooks', cooksRoutes);
 app.use('/headsets', headsetsRoutes);
 app.use('/notebooks', notebooksRoutes);
 app.use('/discontinuos', discontinuosRoutes);
 
-try {
-    await db.authenticate();
-    console.log('Connection has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
+db.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 app.get('/', (req, res) => {
     res.send('Hello World');
--- database/db.js
+++ database/db.js
@@ -1,29 +1,17 @@
-import { Sequelize } from "sequelize";
-import mysql2 from "mysql2";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('aiwa_web', 'aiwa_web', '@BaseSitioAiwa2024@', {
-    host: 'totalhome.xvserver.com',
-    dialect: 'mysql',
-    dialectModule: mysql2,
+dotenv.config();
 
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+    }
+);
 
-export default db;
-
-// import { Sequelize } from "sequelize";
-// import mysql2 from "mysql2";
-// import dotenv from "dotenv";
-
-// // Cargar variables de entorno
-// dotenv.config();
-
-// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT,
-//     dialectModule: mysql2,
-//     //logging: false, // Opcional, para desactivar el registro de SQL en la consola
-// });
-
-// export default db;
-
-
+export default sequelize;
